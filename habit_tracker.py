e6e0b1dec28e96f41ad83834015cf3ec9348ad59
VilMe <vilma.medrano00@gmail.com>
2025-11-29 22:29:10 -0600

    start to define function track_habit

--- habit_tracker.py
+++ habit_tracker.py
@@ -13,6 +13,10 @@ class Habit:
 
 
 
+def track_habit(name:str, start: datetime, cost: float, minutes_used: float) -> Habit:
+    goal: int = 60
+    hourly_wage: int = 30
 
-
+    time_elapsed: float = (datetime.now() - start).total_seconds()
+    
 
