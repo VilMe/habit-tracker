44499cc85ffd75788e688fe8c91ad74172bd3a66
VilMe <vilma.medrano00@gmail.com>
2025-12-01 21:30:00 -0600

    add to habit tracker function

--- habit_tracker.py
+++ habit_tracker.py
@@ -18,5 +18,9 @@ def track_habit(name:str, start: datetime, cost: float, minutes_used: float) -> 
     hourly_wage: int = 30
 
     time_elapsed: float = (datetime.now() - start).total_seconds()
-    
+    hours: float = round(time_elapsed / 60 / 60, 1)
+    days: float = round(hours / 24, 2)
 
+    money_saved: float = cost * days
+    minutes_used: float = round(days * minutes_used)
+    total_money_saved: str = f'$({round(money_saved + (minutes_used / 60 * hourly_wage), 2)})'
