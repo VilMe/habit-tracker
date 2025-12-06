170a8051f5ee2499a723be48a204c91ac8183c8a
VilMe <vilma.medrano00@gmail.com>
2025-12-05 22:53:51 -0600

    define main function in main script

--- main.py
+++ main.py
@@ -4,3 +4,10 @@ from datetime import datetime
 from habit_tracker import track_habit, Habit
 
 
+def main():
+    habits: list[Habit] = [
+        track_habit('Voyager', datetime(2025, 12, 4, 8), cost=10, minutes_used=60)
+    ]
+
+    df = pd.DataFrame(habits)
+    print(tabulate(df, headers='keys', tablefmt='psql'))
